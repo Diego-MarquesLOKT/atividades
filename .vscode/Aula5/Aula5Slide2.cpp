e5026ccbb5e53f35303e15aa743baf031a4eff79
Diego-MarquesLOKT <marques.diegogilb@gmail.com>
2025-06-12 15:43:58 -0300

    porra

--- .vscode/Aula5/Aula5Slide2.cpp
+++ .vscode/Aula5/Aula5Slide2.cpp
@@ -20,7 +20,7 @@
  return 0;
  }*/
 
- void func(float produto, float valor){
+ /*void func(float produto, float valor){
     
     float total;
     while (produto > 0){
@@ -41,4 +41,43 @@
 float produto,valor;
 cin >> produto >> valor;
 func(produto,valor);
+ }*/
+
+/* float valor(int n){
+
+int media= 0;  
+int num;  
+for (int cont=0; cont < n;cont++){
+ 
+    media = media + num;
+cin >> num;
+}
+media = media/n;
+return media;
+ }
+ int main(){
+cout << "Calculador de media (eu acho): " << endl;
+int penes;
+int n;
+penes = valor(n);
+cout << "media é : " << penes;
+    return 0;
+ }*/
+
+ float fatorial(int valor){
+    int cont;
+    int p=1;
+    for(cont =2 ; cont < valor; cont++){
+ p = p*cont;
+
+
+    }
+return p;
+ }
+
+ int main(){
+    int valor, penes;
+    cin >> valor;
+    penes = fatorial(valor);
+    cout << penes ;
  }
