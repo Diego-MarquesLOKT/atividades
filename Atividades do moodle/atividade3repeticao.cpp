d448e8a89f501742bc718df8b6e67434118dd26b
Diego-MarquesLOKT <marques.diegogilb@gmail.com>
2025-06-17 21:20:00 -0300

    atividade 4 graças a Deus

--- Atividades do moodle/atividade3repeticao.cpp
+++ Atividades do moodle/atividade3repeticao.cpp
@@ -1,13 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
+int maior, menor;
 void recebe(int n1, int n2){
-int cont= 0;
-while(n1 != n2){
 
-    
-    n1++;
+if (n1 < n2) {
+    menor = n1;
+    maior = n2;
+} else {
+    menor = n2;
+    maior = n1;
 }
 
-    
+for (int i = menor; i <= maior; ++i) {
+    if (i % 11 == 5) {
+        cout << i << endl;
+    }
+}
+}
+int main(){
+int n1, n2;
+    cin >> n1 >> n2;
+recebe(n1,n2);
+
+
+    return 0;
 }
