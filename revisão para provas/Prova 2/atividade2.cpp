a190786ec4282cc73bde30da515b1a13f12fee5d
Diego-MarquesLOKT <marques.diegogilb@gmail.com>
2025-06-23 15:33:24 -0300

    calor da misera

--- revisão para provas/Prova 2/atividade2.cpp
+++ revisão para provas/Prova 2/atividade2.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main(){
 int n;
+int multiplicador = 0;
 cin >> n;
-for(int cont = 0; cont <= n; cont ++)
-
-
+for(int cont = 1; cont <= n; cont ++){
+    multiplicador = n * cont;
+    cout << n << " x " << cont << " = " << multiplicador <<endl;
+    
+    }
     return 0;
 }
