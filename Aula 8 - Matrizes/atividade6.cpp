ad990c893c23294788e340f3f15f730acea24ef5
Diego-MarquesLOKT <marques.diegogilb@gmail.com>
2025-07-28 14:41:25 -0300

    primeira atividade feita

---
+++ Aula 8 - Matrizes/atividade6.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+float menor(float mat[5][4]){
+    float menornum= 0;
+    for(int i = 0; i < 5;i++){
+
+        for(int j = 0; j< 4; j++){
+            cin >> mat[i][j];
+            if(mat[i][j] < menornum){
+                menornum = mat[i][j];
+            }
+        }
+    }
+    return menornum;
+}
+
+int main(){
+ float mat[5][4];
+ int recebefunc = menor(mat);
+
+    cout << "O menor valor é: " << recebefunc << endl;
+
+
+return 0;
+}
