c344c9f063108ab7bb35e6d1d27fa44a4d3d211a
Diego-MarquesLOKT <marques.diegogilb@gmail.com>
2025-07-31 15:31:26 -0300

    TODAS ATIVIDADES FEITAS

--- aula 9 - Struct/atividade3.cpp
+++ aula 9 - Struct/atividade3.cpp
@@ -12,20 +12,23 @@ struct veiculo{
 int main(){
     veiculo voyage,fordka;
     cout << "Digite as informações do primeiro veiculo (marca,modelo,placa,ano,km,valor) : ";
-    cin >> voyage.marca;
-    cin>> voyage.modelo;
-    cin >> voyage.placa;
+    cin.getline(voyage.marca,100);
+    cin.getline(voyage.modelo,100);
+    cin.getline(voyage.placa,100);
     cin>> voyage.ano;
     cin >> voyage.km;
     cin >> voyage.valor;
+    cin.ignore();
     cout << "Digite as informações do segundo veiculo no mesmo modelo: ";
-    cin >> fordka.marca;
-    cin>> fordka.modelo;
-    cin >> fordka.placa;
+    
+    cin.getline(fordka.marca,100);
+    cin.getline(fordka.modelo,100);
+    cin.getline(fordka.placa,100);
     cin>> fordka.ano;
     cin >> fordka.km;
     cin >> fordka.valor;
     cout << "Informações primeiro veiculo: \n" << "Marca: " << voyage.marca << endl << "Modelo: " << voyage.modelo << endl << "Placa: " << voyage.placa <<endl << "Ano: " << voyage.ano << endl << "Km: " << voyage.km << endl << "Valor" << voyage.valor << endl; 
-    cout << "Informações segundo veiculo: \n" << "Marca: " << fordka.marca << endl << "Modelo: " << fordka.modelo << endl << "Placa: " << fordka.placa << endl<<"Ano: " << fordka.ano << endl << "Km: " << fordka.km << endl << "Valor" << fordka.valor; 
+    cout << "Informações segundo veiculo: \n" << "Marca: " << fordka.marca << endl << "Modelo: " << fordka.modelo << endl << "Placa: " << fordka.placa << endl
+    ,<<"Ano: " << fordka.ano << endl << "Km: " << fordka.km << endl << "Valor" << fordka.valor; 
 return 0;
 }
